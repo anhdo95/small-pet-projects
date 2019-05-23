53c73f26c482b75c7dbe9bbb659bd94255b5ef89
anhdo <dinhanhdo95@gmail.com>
2019-05-23 08:44:38 +0700

    use VueJS lib

--- english-idioms-and-structures/main.js
+++ english-idioms-and-structures/main.js
@@ -1,51 +1,17 @@
-// Use a binding lib in later
-
-$(function() {
-  var EXCEL_PATH_FILE = "./assets/data.json";
-
-  $.getJSON(EXCEL_PATH_FILE, function(data) {
-    renderMenuItems(data);
-    renderSections(data);
-  });
+var app = new Vue({
+  el: "#app",
+  data: {
+    idioms: []
+  }
 });
 
-function createListItem(name) {
-  var template = $("#menu-item-template").html();
+var EXCEL_PATH_FILE = "./assets/data.json";
 
-  return template.replace(/{{ name }}/g, name);
-}
-
-function createIdiomSection(data, examples) {
-  var template = $("#idiom-section-template").html();
-
-  return template
-    .replace(/{{ name }}/g, data.name)
-    .replace(/{{ meaning }}/g, data.meaning)
-    .replace(/{{ examples }}/g, examples);
-}
-
-function renderMenuItems(data) {
-  var html = "";
-
-  data.forEach(function(item) {
-    html += createListItem(item.name);
+fetch(EXCEL_PATH_FILE)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(idioms) {
+    app.idioms = idioms;
+    console.log("app :", app.idioms);
   });
-
-  $("#menu").html(html);
-}
-
-function renderSections(data) {
-  var html = "";
-
-  data.forEach(function(item) {
-    var examples = "";
-
-    item.examples.forEach(function(ex) {
-      examples += createListItem(ex);
-    });
-
-    html += createIdiomSection(item, examples);
-  });
-
-  $("#sections").html(html);
-}
